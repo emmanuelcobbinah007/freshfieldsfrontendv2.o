2fdf0cc6588730a3119c2e6619922a557e82074a
Emmanuel Cobbinah <emmanuelcobbinah707@gmail.com>
2025-01-21 12:15:31 +0000

    kk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import Image from 'next/image';
-
 export default function Home() {
-  return (
-    <div></div>  
-  );
-}
+    return (
+      <div></div>  
+    );
+  }
