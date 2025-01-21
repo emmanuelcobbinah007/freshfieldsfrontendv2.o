055071c0dea3e2438bafdd830b0cdecc74843aaf
Emmanuel Cobbinah <emmanuelcobbinah707@gmail.com>
2025-01-21 12:47:08 +0000

    Trust res

--- src/app/components/Trust.tsx
+++ src/app/components/Trust.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import container from '../../assets/images/trust/Container.png'
 import customerSupport from '../../assets/images/trust/CustomerSupport.png'
 import securePayment from '../../assets/images/trust/SecurePayment.png'
@@ -7,27 +6,27 @@ import { StaticImageData } from 'next/image'
 
 const Trust = () => {
   return (
-    <div className='w-[80%] mx-auto'>
-        <div className='grid grid-cols-4 gap-10 py-10'>
-            <div className='flex flex-col items-center p-3'>
-                <img src={(container as StaticImageData).src} className='w-[30%]' alt='quality' />
-                <h1 className='text-md font-bold mt-5'>Very Fast Delivery</h1>
-                <p className='text-xs text-gray-400 text-center mt-2'>Low Delivery Cost</p>
+    <div className='w-[90%] mx-auto'>
+        <div className='grid grid-cols-2 md:grid-cols-4 gap-4 py-8'>
+            <div className='flex flex-col items-center p-2'>
+                <img src={(container as StaticImageData).src} className='w-[40%] md:w-[25%]' alt='quality' />
+                <h1 className='text-xs md:text-sm font-bold mt-3'>Very Fast Delivery</h1>
+                <p className='text-xs text-gray-400 text-center mt-1'>Low Delivery Cost</p>
             </div>
-            <div className='flex flex-col items-center p-3'>
-                <img src={(customerSupport as StaticImageData).src} className='w-[30%]' alt='organic' />
-                <h1 className='text-md font-bold mt-5'>Great Support 24/7</h1>
-                <p className='text-xs text-gray-400 text-center mt-2'>Access to Instant Contact</p>
+            <div className='flex flex-col items-center p-2'>
+                <img src={(customerSupport as StaticImageData).src} className='w-[40%] md:w-[25%]' alt='organic' />
+                <h1 className='text-xs md:text-sm font-bold mt-3'>Great Support 24/7</h1>
+                <p className='text-xs text-gray-400 text-center mt-1'>Access to Instant Contact</p>
             </div>
-            <div className='flex flex-col items-center p-3'>
-                <img src={(securePayment as StaticImageData).src} className='w-[30%]' alt='eco-friendly' />
-                <h1 className='text-md font-bold mt-5'>100% Secure Payment</h1>
-                <p className='text-xs text-gray-400 text-center mt-2'>We ensure your money is safe</p>
+            <div className='flex flex-col items-center p-2'>
+                <img src={(securePayment as StaticImageData).src} className='w-[40%] md:w-[25%]' alt='eco-friendly' />
+                <h1 className='text-xs md:text-sm font-bold mt-3'>Secure Payment</h1>
+                <p className='text-xs text-gray-400 text-center mt-1'>100% Secure Payment</p>
             </div>
-            <div className='flex flex-col items-center p-3'>
-                <img src={(moneyBack as StaticImageData).src} className='w-[30%]' alt='eco-friendly' />
-                <h1 className='text-md font-bold mt-5'>Cash-Back Guarantee</h1>
-                <p className='text-xs text-gray-400 text-center mt-2'>30 days money-back</p>
+            <div className='flex flex-col items-center p-2'>
+                <img src={(moneyBack as StaticImageData).src} className='w-[40%] md:w-[25%]' alt='money-back' />
+                <h1 className='text-xs md:text-sm font-bold mt-3'>Money Back Guarantee</h1>
+                <p className='text-xs text-gray-400 text-center mt-1'>30 Days Money Back</p>
             </div>
         </div>
     </div>
