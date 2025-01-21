6a30f6dbbd0c46162ffbb2b0259eaefb40441d29
Emmanuel Cobbinah <emmanuelcobbinah707@gmail.com>
2025-01-21 11:49:11 +0000

    navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Navbar from "./components/Navbar";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -25,8 +26,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased font-poppins`}
       >
+        <div className="bg-[#FFC1070F] h-[40px]"></div>
+        <Navbar />
         {children}
       </body>
     </html>
