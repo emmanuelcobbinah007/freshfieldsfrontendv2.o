c632eb048881c0cd97d7c39f72cd7d07e6751a63
Emmanuel Cobbinah <emmanuelcobbinah707@gmail.com>
2025-01-22 11:43:03 +0000

    brough tback components

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import React, { useState } from 'react'
+import logo from '../../assets/images/Logo.png'
+import Link from 'next/link';
+import Image, { StaticImageData } from 'next/image'
+import { CiSearch, CiHeart, CiUser, CiShoppingBasket } from "react-icons/ci";
+import { HiMenu, HiX } from "react-icons/hi";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className='bg-white'>
+      <div className='w-[90%] md:w-[80%] mx-auto'>
+        <div className='flex justify-between items-center py-4'>
+          <Link href='/' passHref>
+            <Image src={logo as StaticImageData} alt='logo' className='h-7 hover:scale-105 duration-300 w-[175px]' />
+          </Link>
+          <div className='hidden md:flex items-center'>
+            <Link href='/' passHref>
+              <p className='px-3 flex items-center hover:text-[#489706] hover:cursor-pointer duration-300 hover:scale-105'>Home</p>
+            </Link>
+            <Link href='/products' passHref>
+            <p className='px-3 flex items-center hover:text-[#489706] hover:cursor-pointer duration-300 hover:scale-105'>Shop</p>
+            </Link>
+            <p className='px-3 flex items-center hover:text-[#489706] hover:cursor-pointer duration-300 hover:scale-105'>Work With Us</p>
+            <p className='px-3 flex items-center hover:text-[#489706] hover:cursor-pointer duration-300 hover:scale-105'>About Us</p>
+          </div>
+          <div className='hidden md:flex items-center'>
+            <CiSearch size={27} className='mx-[5px] hover:bg-gray-200 cursor-pointer rounded-[50%] p-1 duration-300' />
+            <CiHeart size={27} className='mx-[5px] hover:bg-gray-200 cursor-pointer rounded-[50%] p-1 duration-300' />
+            <Link href='/sign-in' passHref>
+            <CiUser size={27} className='mx-[5px] hover:bg-gray-200 cursor-pointer rounded-[50%] p-1 duration-300' />
+            </Link>
+            <CiShoppingBasket size={27} className='mx-[5px] hover:bg-gray-200 cursor-pointer rounded-[50%] p-1 duration-300' />
+          </div>
+          <div className='md:hidden flex items-center'>
+            <button onClick={toggleMenu}>
+              {isOpen ? <HiX size={27} /> : <HiMenu size={27} />}
+            </button>
+          </div>
+        </div>
+        <div className={`fixed top-0 right-0 h-full bg-white shadow-lg transform ${isOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300 ease-in-out md:hidden w-1/2 p-4`}>
+          <div className='flex justify-end'>
+            <button onClick={toggleMenu}>
+              <HiX size={27} />
+            </button>
+          </div>
+          <div className='flex flex-col items-center space-y-4 mt-4'>
+            <Link href='/' passHref>
+              <p className='py-2 px-4 w-full text-center hover:text-[#489706] hover:cursor-pointer duration-300 font-semibold'>Home</p>
+            </Link>
+            <Link href='/products' passHref>
+            <p className='py-2 px-4 w-full text-center hover:text-[#489706] hover:cursor-pointer duration-300 font-semibold'>Shop</p>
+            </Link>
+            <p className='py-2 px-4 w-full text-center hover:text-[#489706] hover:cursor-pointer duration-300 font-semibold'>Work With Us</p>
+            <p className='py-2 px-4 w-full text-center hover:text-[#489706] hover:cursor-pointer duration-300 font-semibold'>About Us</p>
+            <div className='flex items-center py-2'>
+              <CiSearch size={27} className='mx-[5px] hover:bg-gray-200 cursor-pointer rounded-[50%] p-1 duration-300' />
+              <CiHeart size={27} className='mx-[5px] hover:bg-gray-200 cursor-pointer rounded-[50%] p-1 duration-300' />
+              <Link href='/sign-in' passHref>
+            <CiUser size={27} className='mx-[5px] hover:bg-gray-200 cursor-pointer rounded-[50%] p-1 duration-300' />
+            </Link>
+              <CiShoppingBasket size={27} className='mx-[5px] hover:bg-gray-200 cursor-pointer rounded-[50%] p-1 duration-300' />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/app/components/Trust.tsx
@@ -0,0 +1,36 @@
+import container from '../../assets/images/trust/Container.png'
+import customerSupport from '../../assets/images/trust/CustomerSupport.png'
+import securePayment from '../../assets/images/trust/SecurePayment.png'
+import moneyBack from '../../assets/images/trust/MoneyBack.png'
+import { StaticImageData } from 'next/image'
+
+const Trust = () => {
+  return (
+    <div className='w-[90%] mx-auto'>
+        <div className='grid grid-cols-2 md:grid-cols-4 gap-4 py-8'>
+            <div className='flex flex-col items-center p-2'>
+                <img src={(container as StaticImageData).src} className='w-[40%] md:w-[25%]' alt='quality' />
+                <h1 className='text-xs md:text-sm font-bold mt-3'>Very Fast Delivery</h1>
+                <p className='text-xs text-gray-400 text-center mt-1'>Low Delivery Cost</p>
+            </div>
+            <div className='flex flex-col items-center p-2'>
+                <img src={(customerSupport as StaticImageData).src} className='w-[40%] md:w-[25%]' alt='organic' />
+                <h1 className='text-xs md:text-sm font-bold mt-3'>Great Support 24/7</h1>
+                <p className='text-xs text-gray-400 text-center mt-1'>Access to Instant Contact</p>
+            </div>
+            <div className='flex flex-col items-center p-2'>
+                <img src={(securePayment as StaticImageData).src} className='w-[40%] md:w-[25%]' alt='eco-friendly' />
+                <h1 className='text-xs md:text-sm font-bold mt-3'>Secure Payment</h1>
+                <p className='text-xs text-gray-400 text-center mt-1'>100% Secure Payment</p>
+            </div>
+            <div className='flex flex-col items-center p-2'>
+                <img src={(moneyBack as StaticImageData).src} className='w-[40%] md:w-[25%]' alt='money-back' />
+                <h1 className='text-xs md:text-sm font-bold mt-3'>Money Back Guarantee</h1>
+                <p className='text-xs text-gray-400 text-center mt-1'>30 Days Money Back</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Trust
