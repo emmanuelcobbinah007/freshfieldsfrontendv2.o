c1bba712ccb6f8d564b6ec725dc5773a5936edf9
Emmanuel Cobbinah <emmanuelcobbinah707@gmail.com>
2025-01-22 11:41:33 +0000

    took out collections

--- src/app/collections/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-// export default function CollectionPage({ params }: { params: { id: string } }) {
-//     return (
-//       <div>
-//         <h1>Collection ID: {params.id}</h1>
-//       </div>
-//     );
-//   }
